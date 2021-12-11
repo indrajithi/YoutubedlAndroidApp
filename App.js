82121ad97022df61ad5266bab89f07291661b272
Indrajith Indraprastham <indrajith.indraprastham@go-jek.com>
2021-12-11 22:09:59 +0530

    [youtube-dl-react] Fetch downloadable youtube video links

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  FlatList,
+  Text,
+  View,
+  TextInput,
+  Button,
+} from "react-native";
+import { Linking } from "react-native";
+// import responseSample from "./responseSample.json";
+import { NavigationContainer, DarkTheme } from "@react-navigation/native";
 
 export default function App() {
+  const [url, setUrl] = useState("");
+  const [data, setData] = useState('');
+  const [isLoading, setLoading] = useState(false);
+  const apiServer = "https://youtube-235.herokuapp.com/download?url="
+  const fetchVideoInfo = () => {
+    if (url.length > 0) {
+      return fetch(apiServer + url)
+        .then((response) => response.json())
+        .then((json) => {
+          setData(json.formats);
+          setLoading(false);
+          return json;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={ DarkTheme }>
+      <View style={ styles.container }>
+        <>
+          <TextInput
+            style={ {
+              height: 40,
+              borderColor: "gray",
+              borderWidth: 1,
+            } }
+            onChangeText={ (text) => setUrl(text) }
+          />
+          <Button
+            title="Get download links"
+            onPress={ () => fetchVideoInfo() }
+          />
+          { isLoading ? (
+            <ActivityIndicator />
+          ) : (
+            <FlatList
+              data={ data }
+              keyExtractor={ ({ format_id }) => format_id }
+              renderItem={ ({ item }) => (
+                <Text
+                  style={ { color: "blue" } }
+                  onPress={ () => Linking.openURL(item.url) }
+                >
+                  { item.format }
+                </Text>
+              ) }
+            />
+          ) }
+        </>
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 25,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

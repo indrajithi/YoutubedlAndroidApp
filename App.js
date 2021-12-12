e829955c766514000d38ba4010fee7cac1777477
Indrajith Indraprastham <indrajith.indraprastham@go-jek.com>
2021-12-12 21:05:23 +0530

    [youtube-dl-react] Url validation. handeling errors, and UI changes

--- App.js
+++ App.js
@@ -2,88 +2,188 @@ import React, { useState } from "react";
 import {
   ActivityIndicator,
   StyleSheet,
-  FlatList,
-  Text,
   View,
   TextInput,
-  Button,
+  StatusBar,
+  TouchableHighlight,
+  Alert,
 } from "react-native";
-import { Linking } from "react-native";
-// import responseSample from "./responseSample.json";
-import { NavigationContainer, DarkTheme } from "@react-navigation/native";
+// import responseSample from "./responseSample.json"
+// import { Linking } from "react-native"
+import * as Linking from "expo-linking";
+
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Button } from "react-native-elements";
+
+const ALLOWED_HOSTS = [
+  "www.youtube.com",
+  "youtube.com",
+  "youtu.be",
+  "www.youtu.be",
+  "m.youtube.com",
+  "www.m.youtube.com",
+];
+
+function isValidUrl(string) {
+  const url = Linking.parse(string);
+
+  if (!ALLOWED_HOSTS.includes(url.hostname)) return false;
+
+  return url.scheme === "http" || url.scheme === "https";
+}
+
+const DowloadList = ({ audio, video, data }) => {
+  const getSizeInMegaBytes = (format) => {
+    console.log(format);
+    if (!format) return "";
+    return (format.filesize / 1024 / 1024).toFixed(2).toString() + " MB";
+  };
+  const audioSize = getSizeInMegaBytes(audio);
+  const videoSize = getSizeInMegaBytes(video);
+  return (
+    <View style={styles.downloadListContainer}>
+      <View style={{ marginBottom: 20 }}>
+        <Button
+          title={`Download Audio 4.03 MB${audioSize}`}
+          type="outline"
+          onPress={() => Linking.openURL(audio.url)}
+        />
+      </View>
+      <View>
+        <Button
+          title={`Download Video 16.89 MB${videoSize}`}
+          type="outline"
+          onPress={() => Linking.openURL(video.url)}
+        />
+      </View>
+    </View>
+  );
+};
 
 export default function App() {
   const [url, setUrl] = useState("");
-  const [data, setData] = useState('');
+  const [isComplete, setIsComplete] = useState(false);
+  const [data, setData] = useState();
+  const [audio, setAudio] = useState();
+  const [video, setVideo] = useState();
+  const [isError, setIsError] = useState(false);
   const [isLoading, setLoading] = useState(false);
-  const apiServer = "https://youtube-235.herokuapp.com/download?url="
+
+  const apiServer = "https://youtube-235.herokuapp.com/download?url=";
+  const setAudioVideoDownloads = (formats) => {
+    if (formats.length >= 4) {
+      setAudio(formats[3]);
+    }
+    if (formats.length >= 17) {
+      setVideo(formats[16]);
+    }
+    setLoading(false);
+  };
+
+  const alertUser = (title, message) =>
+    Alert.alert(title, message, [{ text: "OK" }]);
+
   const fetchVideoInfo = () => {
+    if (!isValidUrl(url))
+      return alertUser("Invalid URL", "Please enter a valid URL");
+    setLoading(true);
     if (url.length > 0) {
-      return fetch(apiServer + url)
+      return fetch(apiServer + url, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
         .then((response) => response.json())
         .then((json) => {
           setData(json.formats);
-          setLoading(false);
-          return json;
+          setAudioVideoDownloads(json.formats);
+          setIsComplete(true);
         })
-        .catch((error) => {
-          console.error(error);
+        .catch(() => {
+          setLoading(false);
+          setIsComplete(true);
+          setIsError(true);
         });
     }
   };
 
+  const clearEverything = () => {
+    setUrl("");
+    setData(null);
+    setAudio(null);
+    setVideo(null);
+    setLoading(false);
+    setIsComplete(false);
+  };
+
   return (
-    <NavigationContainer theme={ DarkTheme }>
-      <View style={ styles.container }>
-        <>
-          <TextInput
-            style={ {
-              height: 40,
-              borderColor: "gray",
-              borderWidth: 1,
-              padding: 10,
-              minWidth: "70%",
-            } }
-            placeholder="Enter a youtube URL"
-            autoFocus
-            value={ url }
-            onChangeText={ (text) => setUrl(text) }
-          />
-          <View
-          style={ { 
+    <View style={styles.container}>
+      <>
+        <StatusBar animated={true} hidden={false} />
+        <Icon name="cloud-download" size={100} color="#4671e0" />
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ flex: 4 }}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Paste a Youtube link here"
+              placeholderTextColor="#b5bcc9"
+              color="#b5bcc9"
+              padding={10}
+              selectTextOnFocus
+              autoFocus
+              value={url}
+              onChangeText={(text) => setUrl(text)}
+            />
+          </View>
+          <View style={{ position: "absolute", right: 5 }}>
+            <TouchableHighlight
+              style={{ marginTop: 51 }}
+              onPress={() => {
+                isComplete ? clearEverything() : fetchVideoInfo();
+              }}
+              underlayColor="transparent"
+            >
+              <View>
+                <Icon
+                  name={isComplete ? "undo" : "search"}
+                  size={35}
+                  color="#4285F4"
+                />
+              </View>
+            </TouchableHighlight>
+          </View>
+        </View>
+        <View
+          style={{
             paddingTop: 8,
             borderRadius: 4,
             marginHorizontal: "1%",
             marginBottom: 6,
             marginTop: 10,
-            textAlign: "center"}}>
-          <Button
-            title="Get download links"
-            color="forestgreen"
-            onPress={ () => fetchVideoInfo() }
-          />
-          </View>
-          
-          { isLoading ? (
-            <ActivityIndicator />
+            textAlign: "center",
+          }}
+        ></View>
+        <View>
+          {isLoading ? (
+            <ActivityIndicator size="large" color="#00ff00" />
           ) : (
-            <FlatList
-              style={ { padding: 10 } }
-              data={ data }
-              keyExtractor={ ({ format_id }) => format_id }
-              renderItem={ ({ item }) => (
-                <Text
-                  style={ { color: "blue" } }
-                  onPress={ () => Linking.openURL(item.url) }
-                >
-                  { item.format }
-                </Text>
-              ) }
-            />
-          ) }
-        </>
-      </View>
-    </NavigationContainer>
+            <>
+              {data && isComplete && !isError && (
+                <DowloadList audio={audio} video={video} data={data} />
+              )}
+              {isError &&
+                alertUser(
+                  "Error",
+                  "Something went wrong. Try after sometime"
+                ) &&
+                clearEverything()}
+            </>
+          )}
+        </View>
+      </>
+    </View>
   );
 }
 
@@ -94,9 +194,25 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
+    backgroundColor: "#141212",
     padding: 25,
     alignItems: "center",
-    justifyContent: "center",
-    marginTop: "30%",
+    // justifyContent: "center",
+  },
+
+  textInput: {
+    height: 40,
+    minWidth: 300,
+    borderWidth: 1,
+    marginTop: 50,
+    borderColor: "#4285F4",
+  },
+  downloadListContainer: {
+    flex: 1,
+    position: "relative",
+    flexDirection: "column",
+    // justifyContent: "center",
+    padding: 30,
+    // marginRight:30,
   },
 });
